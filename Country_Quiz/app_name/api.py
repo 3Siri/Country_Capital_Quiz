a4768fd3dde6d89d4c7af0fcaf4d93c6db584dce
3Siri <138786856+3Siri@users.noreply.github.com>
2023-07-08 19:32:27 +0530

    api.py

--- Country_Quiz/app_name/api.py
+++ Country_Quiz/app_name/api.py
@@ -1,35 +1,14 @@
 # app_name/api.py
 import requests
 from .models import Country
-"""
-def fetch_country_data():
-    url = 'https://countriesnow.space/api/v0.1/countries/capital'  # Replace with your API endpoint
-    response = requests.get(url)
-    data = response.json()
 
-    for country in data:
-        name = country['name']
-        capital = country['capital']
-        Country.objects.create(name=name, capital=capital)
-
-"""
 def fetch_country_data():
-    url = 'https://countriesnow.space/api/v0.1/countries/capital'  # Replace with your API endpoint
+    url = 'https://countriesnow.space/api/v0.1/countries/capital'  
     response = requests.get(url)
     data_1 = response.json()
 
-    #key_objects = [(country['name'], country['capital']) for country in data_1['data']]  # Extracting name and capital as key objects
- 
-
-
-    #key_objects = [country['name'] for country in data_1['data']]  # Extracting name and capital as key objects
-
-    key_objects = [country['name'] for country in data_1['data']]  # Extracting name and capital as key objects
-    #key_objects_2 = [country['name'] +"-" + country['capital'] for country in data_1['data']] 
-
+    key_objects = [country['name'] for country in data_1['data']]  
     print(type(key_objects))
-    #print(key_objects, key_objects_2)
-    #return key_objects, key_objects_2
     return key_objects
 
 
